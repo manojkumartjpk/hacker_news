fc627144180bfd8082ca408b1931074468ec07d7
Manoj <manoj@peakperformer.io>
2025-12-25 10:34:23 +0530

    release bug

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Suspense } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
@@ -20,7 +21,9 @@ export default function RootLayout({ children }) {
           style={{ margin: '0 auto', backgroundColor: '#f6f6ef' }}
         >
           <tbody>
-            <Header />
+            <Suspense fallback={<tr><td style={{ backgroundColor: '#ff6600', height: '24px' }}>&nbsp;</td></tr>}>
+              <Header />
+            </Suspense>
             <tr style={{ height: '10px' }} />
             <tr>
               <td className="hncontent">
